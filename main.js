4b90b559384dc2e3ef0516b37ead5804c672254d
Marko Stjepanek <marko.stjepanek@gmail.com>
2019-04-08 11:46:56 +0200

    new functions

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
-let listElement, searchbarElement, toDoItems;
+let listElement, searchbarElement;
+
+function getTodosFromLS() {
+  const items = localStorage.getItem('todos');
+  return JSON.parse(items);
+}
+
+function setTodosInLS(item) {
+  localStorage.setItem('todos', JSON.stringify(item));
+}
 
 function getJSON(filePath, callback) {
   listElement = document.getElementById('js-list');
   searchbarElement = document.getElementById('js-searchbar');
 
   fetch(filePath)
-    .then(file => file.json())   //dohvaca json i prebacuje ga u objekt 
-    .then(json => {              //tu radi s tim objektom sada i vrsi se provjera
+    .then(file => file.json())
+    .then(json => {
+      const toDoItems = getTodosFromLS();
+      if (!toDoItems) {
+        setTodosInLS(json.todos);
+      }
+
       if (callback && typeof callback === 'function') {
         callback(json.todos);
       }
@@ -14,19 +28,37 @@ function getJSON(filePath, callback) {
     .catch(error => console.log(error));
 }
 
-function renderTodoCards(todos) {
-  toDoItems = todos;   //array je i ide kroz array
-  todos.forEach(item => {
+function renderTodoCards(toDoItems) {
+  toDoItems.sort(sortByDateAsc).forEach(item => {
     listElement.innerHTML += renderToDoItem(item);
   });
+}
+
+function sortByDateAsc(a, b) {
+  const first = new Date(a.date);
+  const second = new Date(b.date);
+
+  if (first && second) {
+    return first.getTime() - second.getTime();
+  }
+}
+
+function sortByDateDesc(a, b) {
+  const first = new Date(a.date);
+  const second = new Date(b.date);
+
+  if (first && second) {
+    return second.getTime() - first.getTime();
+  }
 }
 
 function clearAllTodoCards() {
   listElement.innerHTML = '';
 }
 
-function filterTodoCards() {
+function searchTodos() {
   const searchQuery = searchbarElement.value;
+  const toDoItems = getTodosFromLS();
 
   if (searchQuery.length > 3) {
     const filteredToDoItems = toDoItems.filter(todoItem =>
@@ -34,7 +66,7 @@ function filterTodoCards() {
     );
 
     clearAllTodoCards();
-    filteredToDoItems.forEach(item => {
+    filteredToDoItems.sort(sortByDateAsc).forEach(item => {
       listElement.innerHTML += renderToDoItem(item);
     });
 
@@ -63,6 +95,8 @@ function renderToDoItem(todo) {
 
 function setCompletedStatus(event) {
   const { mojaVrijednost } = event.currentTarget.dataset;
+
+  let toDoItems = getTodosFromLS();
   toDoItems = toDoItems.map(item => {
     if (item.id === mojaVrijednost) {
       item.completed = !!!item.completed;
@@ -71,43 +105,82 @@ function setCompletedStatus(event) {
 
     return item;
   });
+
+  setTodosInLS(toDoItems);
 }
 
 function addItem() {
   const { value } = searchbarElement;
+  const toDoItems = getTodosFromLS();
 
   if (!value.length) {
     return;
   }
 
-  // Dodati ID
   const newToDo = {
-    id: '',
+    id: getId(),
     title: value,
     completed: false,
-    date: new Date()
+    date: new Date().setMonth(new Date().getMonth() + 3)
   };
 
-  listElement.innerHTML += renderToDoItem(newToDo);
+  let updatedToDos = [...toDoItems, newToDo];
+  updatedToDos = updatedToDos.sort(sortByDateAsc);
+
+  renderTodoCards(updatedToDos);
+  setTodosInLS(updatedToDos);
   searchbarElement.value = '';
 }
 
-function filterCompleted() {    //vjerojatno se moze napraviti te 2 funkc u 1
-  clearAllTodoCards();
-  toDoItems.forEach(item => {
-    if( item.completed === true) {
-      listElement.innerHTML +=renderToDoItem(item) ;
-      } });
-}
-function filterNotCompleted() {
-  clearAllTodoCards();
-  toDoItems.forEach(item => {
-    if( item.completed === false) {
-      listElement.innerHTML +=renderToDoItem(item) ;
-      } });
+function getId() {
+  function S4() {
+    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+  }
+
+  // then to call it, plus stitch in '4' in the third group
+  guid = (
+    S4() +
+    S4() +
+    '-' +
+    S4() +
+    '-4' +
+    S4().substr(0, 3) +
+    '-' +
+    S4() +
+    '-' +
+    S4() +
+    S4() +
+    S4()
+  ).toLowerCase();
+
+  return guid;
 }
 
-function All() {
+function showFilteredByDate(isUpcoming) {
+  const toDoItems = getTodosFromLS();
+  const filteredItems = toDoItems.filter(item => {
+    const currentDateTime = new Date().getTime();
+    const itemDate = new Date(item.date).getTime();
+
+    return isUpcoming ? itemDate > currentDateTime : itemDate < currentDateTime;
+  });
+
+  clearAllTodoCards();
+  renderTodoCards(filteredItems);
+}
+
+function showFilteredByCompletion(isCompleted) {
+  const toDoItems = getTodosFromLS();
+  const filteredItems = toDoItems.filter(item =>
+    isCompleted ? item.completed : !item.completed
+  );
+
+  clearAllTodoCards();
+  renderTodoCards(filteredItems);
+}
+
+function showAll() {
+  const toDoItems = getTodosFromLS();
   clearAllTodoCards();
   renderTodoCards(toDoItems);
 }
